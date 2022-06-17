84d4b7eadc1de81cf0e8d985fdfaddf96e84e555
HAMDIAmenAllah <amen.allah.hamdi@outlook.fr>
2022-06-17 15:21:54 +0200

    Introduction à la sécurité de notre application

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -10,8 +10,11 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CategoryController extends AbstractController
 {
@@ -28,7 +31,7 @@ class CategoryController extends AbstractController
             'categories' => $categories,
         ]);
     } */
-    
+
     /**
      * @Route("/category", name="app_category")
      */
@@ -63,10 +66,48 @@ class CategoryController extends AbstractController
 
     /**
      * @Route("/admin/category/{id}/edit", name="category_edit")
+    
      */
-    public function edit($id, CategoryRepository $categoryRepository, Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
+    // mettre is granted dans l'annotationça dans l'annotation pour gérer les rôles 
+    // @IsGranted("ROLE_ADMIN", message="Vous n'avez pas le droit d'acceder à cette resource")
+    public function edit($id, CategoryRepository $categoryRepository, Request $request, EntityManagerInterface $em, SluggerInterface $slugger, Security $security): Response
     {
+
+        // $this->denyAccessUnlessGranted("ROLE_ADMIN", null, "Vous n'avez pas le droit d'acceder à cette resource");
+        // $user = $security->getUser();
+        // if ($user === null) {
+        //     return $this->redirectToRoute('security_login');
+        // }
+
+        // if (!in_array("ROLE_ADMIN", $user->getRoles())) {
+        //     throw new AccessDeniedHttpException("Vous n'avez pas le droit d'acceder à cette ressource");
+        // }
+
         $category = $categoryRepository->find($id);
+
+        if (!$category) {
+            throw new NotFoundHttpException("Cette catégorie n'existe pas");
+        }
+        /* //début code 0A
+        //récupérer l'utilisateur
+        $user = $this->getUser();
+        // Rediriger su personne n'est connécté
+        if (!$user) {
+            return $this->redirectToRoute('security_login');
+        }
+        // Vérifier si c'est le rateur de la catégorie
+        if ($user !== $category->getOwner()) {
+            throw new AccessDeniedHttpException("vous n'êtes pas le propiétaire de cette catégorie");
+        }
+        // fin code 0A */
+
+        /* // on remplace le code 0A par le voter la ligne ci desous
+        //début voter
+        // $security->isGranted('CAN_EDIT', $category);
+        //ou
+        $this->denyAccessUnlessGranted('CAN_EDIT', $category, "Vous n'êtes pas le propiètaire de cette catégorie");
+        //fin voter */
+
         $form = $this->createForm(CategoryType::class, $category);
 
         $form->handleRequest($request);
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -39,6 +41,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\Column(type="string", length=255)
      */
     private $fullName;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Category::class, mappedBy="owner")
+     */
+    private $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -137,6 +149,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setFullName(string $fullName): self
     {
         $this->fullName = $fullName;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Category>
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories[] = $category;
+            $category->setOwner($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        if ($this->categories->removeElement($category)) {
+            // set the owning side to null (unless already changed)
+            if ($category->getOwner() === $this) {
+                $category->setOwner(null);
+            }
+        }
 
         return $this;
     }
